f84b85c990f27343436659ff6eeece22f305d946
Baghaii <baghaii@gmail.com>
2018-08-03 11:13:14 -0600

    Incorporate Google Cloud Endpoint. Cannot run this due to gradle conflicts.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,14 +1,22 @@
 package com.udacity.gradle.builditbigger;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.sepidehmiller.jokedisplay.JokeDisplayActivity;
 import com.sepidehmiller.jokes.DadJokes;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -46,14 +54,62 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-      String joke = mDadJokes.getJoke();
+
+      new EndpointAsyncTask().execute();
+    /*  String joke = mDadJokes.getJoke();
       Intent intent = new Intent(this, JokeDisplayActivity.class);
       //Set up activity caught by intent filter.
       //https://developer.android.com/guide/components/intents-filters
       intent.setAction(Intent.ACTION_SEND);
       intent.setType("text/plain");
       intent.putExtra(Intent.EXTRA_TEXT, joke);
-      startActivity(intent);
+      startActivity(intent); */
     }
+
+  // https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/77e9910911d5412e5efede5fa681ec105a0f02ad/HelloEndpoints#2-connecting-your-android-app-to-the-backend
+  public class EndpointAsyncTask extends AsyncTask<Void, Void, String> {
+    private MyApi sMyApiService = null;
+
+    @Override
+    protected String doInBackground(Void... params) {
+      if(sMyApiService == null) {  // Only do this once
+        MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+            new AndroidJsonFactory(), null)
+            // options for running against local devappserver
+            // - 10.0.2.2 is localhost's IP address in Android emulator
+            // - turn off compression when running against local devappserver
+            .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+            .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+              @Override
+              public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                abstractGoogleClientRequest.setDisableGZipContent(true);
+              }
+            });
+        // end options for devappserver
+
+        sMyApiService = builder.build();
+      }
+
+
+      try {
+        return sMyApiService.getJoke().execute().getData();
+      } catch (IOException e) {
+        return e.getMessage();
+      }
+    }
+
+    @Override
+    protected void onPostExecute(String joke) {
+      super.onPostExecute(joke);
+      Intent intent = new Intent(MainActivity.this, JokeDisplayActivity.class);
+      //Set up activity caught by intent filter.
+      //https://developer.android.com/guide/components/intents-filters
+      intent.setAction(Intent.ACTION_SEND);
+      intent.setType("text/plain");
+      intent.putExtra(Intent.EXTRA_TEXT, joke);
+      startActivity(intent);
+
+    }
+  }
 
 }
